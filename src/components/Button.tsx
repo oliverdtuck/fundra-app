333f4fcd24f22f2bf8ea523b88de031509f37a98
Oliver Tuck <oliver@elitecoders.co.uk>
2025-09-23 22:02:29 +0100

    Adjust border radius

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,7 @@ interface ButtonProps {
 
 export const Button: FC<ButtonProps> = ({ children, type }) => (
   <button
-    className="rounded-full bg-blue-500 px-4 py-2 font-medium text-white"
+    className="rounded-lg bg-blue-500 px-4 py-2 font-medium text-white"
     type={type}
   >
     {children}
