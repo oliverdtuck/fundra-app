4f105276e159904ca9bfbfcb2033f253bdfccd82
Oliver Tuck <oliver@elitecoders.co.uk>
2025-09-27 15:03:47 +0100

    Add redirects

--- src/routes/_protected/index.tsx
+++ src/routes/_protected/index.tsx
@@ -1,9 +1,57 @@
 import type { FC } from 'react';
 
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, redirect } from '@tanstack/react-router';
+
+import { companiesSuspenseQueryOptions } from '../../hooks/useCompaniesSuspenseQuery';
+import { companyThesesSuspenseQueryOptions } from '../../hooks/useCompanyThesesSuspenseQuery';
 
 const Component: FC = () => <div>Home</div>;
 
 export const Route = createFileRoute('/_protected/')({
+  beforeLoad: async ({ context }) => {
+    const { queryClient } = context;
+    const companies = await queryClient.ensureQueryData(
+      companiesSuspenseQueryOptions()
+    );
+
+    if (companies.length === 0) {
+      // eslint-disable-next-line @typescript-eslint/only-throw-error
+      throw redirect({
+        to: '/onboarding/company-information'
+      });
+    }
+
+    const [
+      { fundingRound, id, productsAndServices, subSector, targetCustomers }
+    ] = companies;
+    const companyTheses = await queryClient.ensureQueryData(
+      companyThesesSuspenseQueryOptions(id)
+    );
+
+    if (!subSector) {
+      // eslint-disable-next-line @typescript-eslint/only-throw-error
+      throw redirect({
+        to: '/onboarding/sector'
+      });
+    }
+
+    if (!fundingRound) {
+      // eslint-disable-next-line @typescript-eslint/only-throw-error
+      throw redirect({
+        to: '/onboarding/funding-stage'
+      });
+    }
+
+    if (
+      !productsAndServices ||
+      !targetCustomers ||
+      companyTheses.length === 0
+    ) {
+      // eslint-disable-next-line @typescript-eslint/only-throw-error
+      throw redirect({
+        to: '/onboarding/overview'
+      });
+    }
+  },
   component: Component
 });
