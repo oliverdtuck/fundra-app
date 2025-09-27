f4384ac2defc79822c8e55038c75b0c6a939e4cd
Oliver Tuck <oliver@elitecoders.co.uk>
2025-09-27 15:48:24 +0100

    Adjust copy

--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -11,19 +11,19 @@ export const signUpSchema = z.object({
     }
 
     if (!/[a-z]/.test(password)) {
-      failed.push('one lowercase letter');
+      failed.push('a lowercase letter');
     }
 
     if (!/[A-Z]/.test(password)) {
-      failed.push('one uppercase letter');
+      failed.push('an uppercase letter');
     }
 
     if (!/\d/.test(password)) {
-      failed.push('one number');
+      failed.push('a number');
     }
 
     if (!/\W/.test(password)) {
-      failed.push('one special character');
+      failed.push('a special character');
     }
 
     if (failed.length > 0) {
